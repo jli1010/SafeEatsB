a9e3de003fdd04a9c0fd81d1bd96130043f41672
Thalia Rodriguez <thalia03@ad.unc.edu>
2024-03-26 16:20:48 -0400

    3

---
+++ App.js
@@ -0,0 +1,60 @@
+import { StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+// Pages
+import Main from './pages/Main';
+import TabNav from './tab/TabNav';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator 
+      screenOptions={{
+        headerShown: false
+      }}>
+      <Stack.Screen
+        name="Main"
+        component={Main}
+      />
+      <Stack.Screen 
+        name="TabNav"
+        component={TabNav}
+      />
+    </Stack.Navigator>
+    </NavigationContainer>);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  appName: {
+    fontSize: 30
+  },
+  textField: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: "80%",
+    margin: 10,
+    padding: 5
+  },
+  checkboxContainer: {
+    marginBottom: 20,
+    flexDirection: 'row',
+
+  },
+  checkboxColumn: {
+    flexDirection: 'column',
+    margin: 2,
+  },
+  checkboxRow: {
+    margin: 5,
+  }
+});
